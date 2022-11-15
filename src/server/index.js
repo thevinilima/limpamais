211a2741d90488484a335cc6f4c143c9755bf2ab
Vinicius Lima <thelimavinicius@gmail.com>
2022-11-15 19:41:47 -0300

    feat(api): add cors

--- src/server/index.js
+++ src/server/index.js
@@ -1,10 +1,13 @@
 import router from './router.js';
 import express from 'express';
+import cors from 'cors';
+
 const PORT = 3003;
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 app.use(router);
 
