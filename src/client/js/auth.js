5a977615bd1c00b08737bb4c966c5c8f3ad0b95f
gabriel <gabrielaugustosb13@gmail.com>
2022-11-22 18:10:21 -0300

    feat(register user): make front comunicate properly with

--- src/client/js/auth.js
+++ src/client/js/auth.js
@@ -1,24 +1,21 @@
-let formRegister = document.querySelector(".sign-up-form")
+let formRegister = document.querySelector('.sign-up-form');
 
-formRegister.addEventListener("submit", async function(e){
-    e.preventDefault()
-    const form = new FormData(e.target)
-    const formData = Object.fromEntries(form.entries())
-    //formData.delete('user-type');
-    try{
-        const response = await fetch("http://localhost:3000/users", {
+formRegister.addEventListener('submit', async function (e) {
+  e.preventDefault();
+  const form = new FormData(e.target);
+  const formData = Object.fromEntries(form.entries());
+  //formData.delete('user-type');
+  try {
+    const response = await fetch('http://localhost:3000/users', {
+      method: 'POST',
+      body: JSON.stringify(formData),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
 
-            method: 'POST',
-            body: formData
-        });
-    
-
-    }
-    catch(error){
-        console.log(error)
-
-    }
-
- 
+    return response;
+  } catch (error) {
+    console.log(error);
+  }
 });
-
