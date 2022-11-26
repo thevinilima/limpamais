ebe5dd76401c49c14d19175fe4c82b30500c13bf
gabriel <gabrielaugustosb13@gmail.com>
2022-11-26 18:48:23 -0300

    first get services implemntations

--- src/client/js/diarista.js
+++ src/client/js/diarista.js
@@ -1,11 +1,23 @@
+const botaoModal = document.getElementById('criaServico');
 
-const botaoModal = document.getElementById("criaServico");
-
-botaoModal.addEventListener("click", function (){
-
-    window.location.reload();
-
+botaoModal.addEventListener('click', function () {
+  window.location.reload();
 });
 
+window.onload = async () => {
+  const token = localStorage.getItem('token');
+  if (!token) return;
 
+  const response = await fetch('http://localhost:3003/services', {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${token}`,
+    },
+  }).then((res) => {
+    res = res.rows;
+    res.status(200).json(response);
+  });
 
+  localStorage.setItem('user', JSON.stringify(user));
+};
