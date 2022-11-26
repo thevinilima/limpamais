ed01358aea1955fb7dcfea61606adc009cb61b79
gabriel <gabrielaugustosb13@gmail.com>
2022-11-26 14:02:36 -0300

    fix input field id

--- src/client/js/auth.js
+++ src/client/js/auth.js
@@ -1,28 +1,28 @@
-let formRegister = document.querySelector(".sign-up-form");
+let formRegister = document.querySelector('.sign-up-form');
 
-formRegister.addEventListener("submit", async function (e) {
+formRegister.addEventListener('submit', async function (e) {
   e.preventDefault();
   const form = new FormData(e.target);
   const formData = Object.fromEntries(form.entries());
 
   try {
-    const response = await fetch("http://localhost:3003/users", {
-      method: "POST",
+    const response = await fetch('http://localhost:3003/users', {
+      method: 'POST',
       body: JSON.stringify(formData),
       headers: {
-        "Content-Type": "application/json",
+        'Content-Type': 'application/json',
       },
     });
 
-    alert('Usuário cadastrado com sucesso!')
+    alert('Usuário cadastrado com sucesso!');
     return response;
   } catch (error) {
     console.error(error.message);
   }
 });
 
-let formLogin = document.querySelector("#form_login");
-formLogin.addEventListener("submit", async function (e) {
+let formLogin = document.querySelector('#form_login');
+formLogin.addEventListener('submit', async function (e) {
   e.preventDefault();
   const loginForm = new FormData(e.target);
   const loginFormData = Object.fromEntries(loginForm.entries());
@@ -30,23 +30,23 @@ formLogin.addEventListener("submit", async function (e) {
   parseInt(loginFormData.tel);
 
   try {
-    const data = await fetch("http://localhost:3003/login ", {
-      method: "POST",
+    const data = await fetch('http://localhost:3003/login ', {
+      method: 'POST',
       body: JSON.stringify(loginFormData),
       headers: {
-        "Content-Type": "application/json",
+        'Content-Type': 'application/json',
       },
     }).then((res) => res.json());
 
-    localStorage.setItem("token", data.token);
+    localStorage.setItem('token', data.token);
     await loadUserData();
 
-    const path = location.pathname.split("/");
-    if (path.slice(-1) === "") path.pop();
+    const path = location.pathname.split('/');
+    if (path.slice(-1) === '') path.pop();
     path.pop();
-    path.push("pages", "contratante.html");
-    location.pathname = path.join("/");
+    path.push('pages', 'contratante.html');
+    location.pathname = path.join('/');
   } catch (error) {
-    console.error(error.message);
+    alert(error.message);
   }
 });
