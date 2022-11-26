0df0e15b5a83544c40bdb40006d0a0e739cc8aba
gabriel <gabrielaugustosb13@gmail.com>
2022-11-26 17:08:46 -0300

    add ingles ver login verification

--- src/client/js/contratante.js
+++ src/client/js/contratante.js
@@ -1,12 +1,11 @@
 const root = document.getElementById('root');
 
 const IsNotLogged = () => {
-  console.log('caiu aq app');
   if (!localStorage.getItem('token')) {
-    return ` <p>Vocẽ não está logado, por favor volte para a tela de login e coloque suas credenciais</p>`;
+    root.innerHTML = ` <p>Vocẽ não está logado, por favor volte para a tela de login e coloque suas credenciais</p>`;
   }
 };
 
 window.onload = () => {
-  root.innerHTML = IsNotLogged();
+  IsNotLogged();
 };
