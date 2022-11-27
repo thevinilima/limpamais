5164ea8703deab7d6780d97920b38c5ae8ff57d2
gabriel <gabrielaugustosb13@gmail.com>
2022-11-27 13:41:44 -0300

    front get services from rewuester

--- src/client/js/contratante.js
+++ src/client/js/contratante.js
@@ -9,13 +9,14 @@ const IsNotLogged = () => {
 
 window.onload = () => {
   IsNotLogged();
+  getRequesterServices();
 };
 
 let btnCreateService = document.querySelector('.butaoSolicita');
 btnCreateService.addEventListener('click', async function (e) {
   e.preventDefault();
   let container = document.querySelector('#main');
-  let loading = document.createElement('P');
+  let loading = document.createElement('h1');
   let textloading = document.createTextNode('Carregando...');
   loading.appendChild(textloading);
   container.appendChild(loading);
@@ -51,7 +52,102 @@ btnCreateService.addEventListener('click', async function (e) {
 
     loading.remove();
     alert(response.message);
+    window.location.reload();
   } catch (error) {
     alert(error.message);
   }
 });
+
+const getRequesterServices = async () => {
+  let container = document.querySelector('#main');
+  let loading = document.createElement('h1');
+  let textloading = document.createTextNode('Carregando...');
+  loading.appendChild(textloading);
+  container.appendChild(loading);
+
+  const token = localStorage.getItem('token');
+  if (!token) return;
+
+  const userTel = JSON.parse(localStorage.getItem('user')).telefone;
+  let response;
+  try {
+    response = await fetch('http://localhost:3003/services/requester', {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${token}`,
+        tel: userTel,
+      },
+    }).then((response) => response.json());
+  } catch (err) {
+    console.log(err.message);
+  }
+
+  response = response.services.rows;
+  localStorage.setItem('servicesAvaiable', JSON.stringify(response));
+  loading.remove();
+  generateServicosCards();
+};
+
+const generateServicosCards = () => {
+  const container = document.getElementById('main');
+  const cards = JSON.parse(localStorage.getItem('servicesAvaiable'));
+
+  for (let i = 0; cards.length; i++) {
+    let card = document.createElement('DIV');
+    let num_servico_criado = document.createElement('H1');
+    let num_servico_criadoT = document.createTextNode(
+      `Serviço: ${cards[i].num_servico_criado}`
+    );
+    num_servico_criado.appendChild(num_servico_criadoT);
+    card.appendChild(num_servico_criado);
+    let bairro = document.createElement('P');
+    let bairroT = document.createTextNode(`Bairro: ${cards[i].bairro}`);
+    bairro.appendChild(bairroT);
+    card.appendChild(bairro);
+    let cep = document.createElement('P');
+    let cepT = document.createTextNode(`CEP: ${cards[i].cep}`);
+    cep.appendChild(cepT);
+    card.appendChild(cep);
+    let cidade = document.createElement('P');
+    let cidadeT = document.createTextNode(`Cidade: ${cards[i].cidade}`);
+    cidade.appendChild(cidadeT);
+    cidade.appendChild(bairro);
+    let comodos = document.createElement('P');
+    let comodosT = document.createTextNode(`Cômodos: ${cards[i].comodos}`);
+    comodos.appendChild(comodosT);
+    card.appendChild(comodos);
+    let descricao_atividade = document.createElement('P');
+    let descricao_atividadeT = document.createTextNode(
+      `Descrição: ${cards[i].descricao_atividade}`
+    );
+    descricao_atividade.appendChild(descricao_atividadeT);
+    card.appendChild(descricao_atividade);
+    let logradouro = document.createElement('P');
+    let logradouroT = document.createTextNode(
+      `Logradouro ${cards[i].logradouro}`
+    );
+    logradouro.appendChild(logradouroT);
+    card.appendChild(logradouro);
+    let numero = document.createElement('P');
+    let numeroT = document.createTextNode(`Número: ${cards[i].numero}`);
+    numero.appendChild(numeroT);
+    card.appendChild(numero);
+    let uf = document.createElement('P');
+    let ufT = document.createTextNode(`UF: ${cards[i].uf}`);
+    uf.appendChild(ufT);
+    card.appendChild(uf);
+    let valor = document.createElement('P');
+    let valorT = document.createTextNode(`Valor: ${cards[i].valor}`);
+    valor.appendChild(valorT);
+    card.appendChild(valor);
+    let data_horario = document.createElement('P');
+    let data_horarioT = document.createTextNode(
+      `Data e Horário: ${cards[i].data_horario}`
+    );
+    data_horario.appendChild(data_horarioT);
+    card.appendChild(data_horario);
+    card.classList = 'card';
+    container.appendChild(card);
+  }
+};
