d247378a99a174842c8160c3ae3250b002075775
Vinicius Lima <thelimavinicius@gmail.com>
2022-11-28 13:12:40 -0300

    fix cnpj disabled

--- src/client/index.js
+++ src/client/index.js
@@ -2,8 +2,10 @@ const sign_in_btn = document.querySelector('#sign-in-btn');
 const sign_up_btn = document.querySelector('#sign-up-btn');
 const container = document.querySelector('.container');
 const inputs = document.querySelectorAll('input[name="user-type"]');
-const cpf = document.querySelector('#cpf');
-const cnpj = document.querySelector('#cnpj');
+const cpfContainer = document.querySelector('#cpf');
+const cnpjContainer = document.querySelector('#cnpj');
+const cpfInput = document.querySelector('#cpf input');
+const cnpjInput = document.querySelector('#cnpj input');
 
 sign_up_btn.addEventListener('click', () => {
   container.classList.add('sign-up-mode');
@@ -13,23 +15,26 @@ sign_in_btn.addEventListener('click', () => {
   container.classList.remove('sign-up-mode');
 });
 
-inputs.forEach((input) => input.addEventListener('change', change));
+inputs.forEach(input => input.addEventListener('change', change));
 
 const userTypes = {
   diarista: () => {
-    cnpj.classList.add('hidden');
-    cpf.classList.remove('hidden');
-    cnpj.setAttribute('disabled', true);
+    cnpjContainer.classList.add('hidden');
+    cnpjInput.setAttribute('disabled', true);
+    cpfContainer.classList.remove('hidden');
+    cpfInput.removeAttribute('disabled');
   },
   cpf: () => {
-    cnpj.classList.add('hidden');
-    cpf.classList.remove('hidden');
-    cnpj.setAttribute('disabled', true);
+    cnpjContainer.classList.add('hidden');
+    cnpjInput.setAttribute('disabled', true);
+    cpfContainer.classList.remove('hidden');
+    cpfInput.removeAttribute('disabled');
   },
   cnpj: () => {
-    cnpj.classList.remove('hidden');
-    cpf.classList.add('hidden');
-    cpf.setAttribute('disabled', true);
+    cpfContainer.classList.add('hidden');
+    cpfInput.setAttribute('disabled', true);
+    cnpjContainer.classList.remove('hidden');
+    cnpjInput.removeAttribute('disabled');
   },
 };
 
