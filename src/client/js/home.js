26bc9a1d0a070b148be3aacee9059b4c327d870c
mviniccius <marcus.viniccius@hotmail.com>
2022-12-07 14:12:55 -0300

    improve diarista styling

--- src/client/js/home.js
+++ src/client/js/home.js
@@ -10,6 +10,15 @@ fecharModal.addEventListener('click', function () {
   let fechar = document.querySelector('.modalInsercao');
 
   fechar.style.display = 'none';
+
+  const inputs = document.querySelectorAll('input');
+  inputs.forEach(input => {
+    input.value = '';
+  });
+  const button = document.querySelector('#serviceActionBtn');
+  button?.removeAttribute('onclick');
+  button?.removeAttribute('class');
+  button.innerHTML = button.dataset.defaultText;
 });
 
 const criaSolicitacao = document.getElementById('criaSolicitacao');
